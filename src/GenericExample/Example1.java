f708f0aaa4959c5bb798e906c1f0288fb8ac8cfb
Mayi21 <May_i21@163.com>
2022-03-11 10:40:45 +0800

    hello world

---
+++ src/GenericExample/Example1.java
@@ -0,0 +1,11 @@
+package GenericExample;
+
+/**
+ * @Author xaohii
+ * @Date 2022/3/11 10:39
+ */
+public class Example1 {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
